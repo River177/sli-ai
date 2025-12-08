4367ba99b4b5dcb064eff3630fb022f68f2dd60b
Elecmonkey <elecmonkey@hotmail.com>
2025-12-09 01:12:34 +0800

    feat(frontend): add UnoCSS support

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import './styles/main.css';
+import 'virtual:uno.css'
 
 const router = createRouter({
   history: createWebHistory(),
